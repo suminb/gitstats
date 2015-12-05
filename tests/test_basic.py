0a4f17b80d3627faf74dc586fcd196dd9e627b33
Sumin Byeon <suminb@gmail.com>
2015-12-05 17:51:27 +0900

    Write some test cases

---
+++ tests/test_basic.py
@@ -0,0 +1,18 @@
+from gitstats import generate_git_log, parse_log_row
+
+
+def test_generate_git_log():
+    """Ensures generate_git_log() works as intended."""
+
+    # Extract logs for the current repository
+    logs = generate_git_log('.')
+    assert len(logs) > 0
+    assert len(logs[0]) == 3
+
+
+def test_parse_log_row():
+    """Ensujres parse_log_row() works as intended."""
+
+    log = 'Sumin Byeon|suminb@gmail.com|Sat Dec 5 17:10:45 2015 +0900'
+    columns = parse_log_row(log)
+    assert len(columns) == 3
