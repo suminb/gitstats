29e32c7e8c4491a018316d1da38799c174ca5f83
Sumin Byeon <suminb@gmail.com>
2015-12-05 16:23:24 +0900

    Rename a file & minor code refactoring

--- gitstats.py
+++ gitstats.py
@@ -2,11 +2,13 @@ __author__ = 'Sumin Byeon'
 __email__ = 'suminb@gmail.com'
 __version__ = '0.1.1'
 
-from dateutil.parser import parse as parse_datetime
-
 import subprocess
-import os, sys
+import os
+# import StringIO
+import sys
 import logging
+
+from dateutil.parser import parse as parse_datetime
 
 logger = logging.getLogger('gitstat')
 #handler = logging.FileHandler('gitstat.log')
@@ -109,9 +111,10 @@ def sort_by_year(log):
 
 def make_svg_report(log, global_max, out=sys.stdout):
     """
-    :param log: parsed log
+    :param log: parsed log for a particular year
     :type log: dict
     :param global_max: global maximum of the number of commits at any given day
+    :type global_max: int
     """
 
     def average_colors(color1, color2):
@@ -141,15 +144,15 @@ def make_svg_report(log, global_max, out=sys.stdout):
     density_offset = global_max * 0.25
 
     for week in range(52):
-        out.write('<g transform="translate(%d, 0)">' % (week*12))
+        out.write('<g transform="translate(%d, 0)">' % (week * 12))
         for day in range(7):
             count_mine, count_others = 0, 0
             try:
-                count_mine = daily_commits_mine[week*7 + day]
+                count_mine = daily_commits_mine[week * 7 + day]
             except:
                 pass
             try:
-                count_others = daily_commits_others[week*7 + day]
+                count_others = daily_commits_others[week * 7 + day]
             except:
                 pass
 
@@ -158,11 +161,11 @@ def make_svg_report(log, global_max, out=sys.stdout):
             density_others = float(count_others + density_offset) / (global_max + density_offset) \
                 if count_others > 0 else 0.0
 
-            color_mine = (238 - density_mine*180, 238 - density_mine*140, 238)
-            color_others = (238, 238 - density_others*180, 238 - density_others*140)
+            color_mine = (238 - density_mine * 180, 238 - density_mine * 140, 238)
+            color_others = (238, 238 - density_others * 180, 238 - density_others * 140)
 
             out.write('<rect class="day" width="10px" height="10px" y="%d" style="fill: #%s"/>' \
-                % (day*12, make_colorcode(average_colors(color_mine, color_others))))
+                % (day * 12, make_colorcode(average_colors(color_mine, color_others))))
         out.write('</g>')
 
     out.write('</svg>')
