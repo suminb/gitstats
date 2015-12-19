d3aa9b7d42d20caee69ad8dc1b10c80b9528a928
Sumin Byeon <suminb@gmail.com>
2015-12-19 20:49:39 +0900

    Add a test case for sort_by_year()

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,16 @@
-from gitstats import generate_git_log, parse_log_row
+from datetime import datetime
+
+from gitstats import generate_git_log, parse_log_row, sort_by_year
+
+
+def validate_log_row(columns):
+    """
+    :param columns: (name, email, datetime) tuple
+    """
+    assert len(columns) == 3
+    assert isinstance(columns[0], str)
+    assert isinstance(columns[1], str)
+    assert isinstance(columns[2], datetime)
 
 
 def test_generate_git_log():
@@ -15,4 +27,22 @@ def test_parse_log_row():
 
     log = 'John Doe|john.doe@gmail.com|Sat Dec 5 17:10:45 2015 +0900'
     columns = parse_log_row(log)
-    assert len(columns) == 3
+    validate_log_row(columns)
+
+
+def test_sort_by_year():
+    """Ensures sort_by_year() works as intended."""
+
+    # Extract logs for the current repository
+    logs = generate_git_log('.')
+    sorted_logs = sort_by_year(logs)
+    years = sorted_logs.keys()
+
+    assert 2012 not in years
+    assert 2013 in years
+    assert 2014 not in years
+    assert 2015 in years
+
+    for year in (2013, 2015):
+        for row in sorted_logs[year]:
+            validate_log_row(row)
